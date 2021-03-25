545d75a0f07c750d831e6dd39e4c6e3ae16e8e29
casseDeChameau <48219695+casseDeChameau@users.noreply.github.com>
2021-03-25 16:01:45 +0100

    site ok à 98% visuellement, potentiellemnt 50% en terme de temps tellement plsu ça va plus je mets du temps à finir un truc, slider property disparait au responsive, il a le droit c'est pas encore 22h

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,19 @@
+// ? change l'état de la nav bar au scroll
 let nav = document.querySelector('.nav-main');
 window.addEventListener('scroll', () => {
     let currentScroll = scrollY;
     if (currentScroll > 200) {
         nav.classList.add('on-scroll');
-        $('.nav-main-links a').css('color', '#000').add('on-scroll');
+        $('.nav-main-links a').addClass('on-scroll');
+        $('.burger-btn .stripe').addClass('on-scroll');
+
     } else {
         nav.classList.remove('on-scroll');
-        $('.nav-main-links a').css('color', '#fff').remove('on-scroll');
+        $('.nav-main-links a').removeClass('on-scroll')
+        $('.burger-btn .stripe').removeClass('on-scroll');
     }
 });
+//? comportement du swiper en header index
 let swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     spaceBetween: 30,
@@ -17,4 +22,21 @@ let swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination',
         clickable: true,
     }
+});
+//? redirections href
+$('.bw-houses .bloc-content').click(() => {
+    window.location.href = "property-single.html";
+});
+$('.bw-news img').click(() => {
+    window.location.href = "single.html";
+});
+$('.nav-main-logo').click(() => {
+    window.location.href = "index.html";
+});
+
+//? comportement burger menu
+$('.burger-btn').click(() => {
+    console.log('click');
+    $('.nav-main-links ul').toggleClass('toggle-class');
+    $('.burger-btn .stripe').toggleClass('on-click')
 });
