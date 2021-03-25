23261c0c0fb67453be446adc7b49ccf302e54ad5
casseDeChameau <48219695+casseDeChameau@users.noreply.github.com>
2021-03-25 11:02:58 +0100

    swiper header, zoom agent hover, scale topbtn, general resolution resizing

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,20 @@
 let nav = document.querySelector('.nav-main');
 window.addEventListener('scroll', () => {
     let currentScroll = scrollY;
-    console.log(scrollY);
     if (currentScroll > 200) {
         nav.classList.add('on-scroll');
         $('.nav-main-links a').css('color', '#000').add('on-scroll');
-        console.log($('.nav-main-links'));
     } else {
         nav.classList.remove('on-scroll');
         $('.nav-main-links a').css('color', '#fff').remove('on-scroll');
+    }
+});
+let swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
     }
 });
